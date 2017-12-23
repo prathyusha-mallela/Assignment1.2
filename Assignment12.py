b39bc77cdb9d71c1aa04d7c32c0046505cbd147a
prathyusha-mallela <pratyushamv@gmail.com>
2017-12-23 17:38:28 +0530

    Update Assignment12.py

--- Assignment12.py
+++ Assignment12.py
@@ -4,6 +4,7 @@ Created on Sat Dec 23 17:20:56 2017
 
 @author: Prathyusha Mallela
 """
+#display list of numbers between 2000 and 3200 where they are multiples of 7 and not 5
 def filter_for_mult7_notin5(lower_range,upper_range):
     a=list(range(lower_range,upper_range+1))
     res=list(filter(lambda x: (x%7==0) & (x%5!=0),a))
